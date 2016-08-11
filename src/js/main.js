f17cdf0a867ef88a856e9fc70dffb9f602393cae
Alias M6 <aliasmontoya719@gmail.com>
2016-08-11 09:12:36 -0600

    added a background for orbs to be positioned on

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
   // defines canvas width and height as well as functions to be used for game
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render:render });
 
 // var crystalProperties = {
 //   startingCrystals = 6;
@@ -39,9 +39,11 @@ function create() {
   hex = game.add.sprite(400, 300, 'hex');
   // allows for orbs to have physical body
   hex.enableBody = true;
+
   // Anchors Orbs to Center
   hex.anchor.setTo(0.5, 0.5);
   game.physics.enable(hex, Phaser.Physics.ARCADE);
+  // hex.body.setSize(42,42,50,0)
 
   // creates rocks to be projected at player orb
   rocks = [];
@@ -66,27 +68,47 @@ function update() {
 
 
     hex.body.angularVelocity = 0;
+
     rocks.forEach(function (rock) {
+
+
       rock.body.angularVelocity = 100;
-      var rand = Math.floor(Math.random() * 10);
+
+      var rand = Math.floor(Math.random() * 100);
       var direction = (rand >= 5) ? -1 : 1;
-      var direction1 = (rand >= 5) ? -1 : 1;
+
+
 
       if ( !rock.body.velocity.x && !rock.body.velocity.y ) {
-        rock.body.velocity.x = rand * direction * 20;
-        rock.body.velocity.y = rand * direction1 * 20;
+        rock.body.velocity.x = rand * direction * 30;
+        rock.body.velocity.y = rand * direction * 30;
+        game.physics.arcade.moveToXY(
+          rock, 400, 300
+        );
       }
 
       if (rock.x < 0) {
           rock.x = game.width;
+          game.physics.arcade.moveToXY(
+            rock, 400, 300
+          );
       } else if (rock.x > game.width) {
           rock.x = 0;
+          game.physics.arcade.moveToXY(
+            rock, 400, 300
+          );
       }
 
       if (rock.y < 0) {
           rock.y = game.height;
+          game.physics.arcade.moveToXY(
+            rock, 400, 300
+          );
       } else if (rock.y > game.height) {
           rock.y = 0;
+          game.physics.arcade.moveToXY(
+            rock, 400, 300
+          );
       }
     })
 
@@ -101,28 +123,12 @@ function update() {
         hex.body.angularVelocity += 100;
     }
 
-    // crystals.body.angularCelocity
-  // if (cursors.left.isDown)
-  // {
-  // hex.angle -= 5;
-  // }
-  // else if (cursors.right.isDown)
-  // {
-  //   hex.angle += 5;
-  // }
-
-  // crystals.rotation += .05
-  // crystals.body.velocity.x = 0;
-  // crystals.body.velocity.y = 0;
-  // crystals.body.angularVelocity = 0;
-  //
-  // crystals.body.angularVelocity + 200;
-
-  // crystals.body.angularVelocity -200
-
-  // crystals.forEach(game.physics.arcade.moveToXY, 400, 300, game.physics.arcade, false, 200);
 
 
+}
+
+function render() {
+  game.debug.body(hex)
 }
 
 
