d793eb363b0bad14f7e190d2ae4686654cda20b2
Alias M6 <aliasmontoya719@gmail.com>
2016-08-17 18:16:05 -0600

    begin preload refactor starting by making new js file for all preload functions

---
+++ src/js/preload.js
@@ -0,0 +1,15 @@
+// preloading player controlled hex
+game.load.image('hex', 'images/hexagon1.png');
+game.load.image('box','images/background/black.png')
+// preloading projectiles
+
+for (let i = 1; i <= 6; i++) {
+
+  game.load.image('rock' + i, 'images/projectiles/rock-' + i + '.png');
+}
+
+for (let i = 1; i <= 4; i++) {
+
+  game.load.image('orb' + i, 'images/orb/orb-' + i + '.png');
+
+}
